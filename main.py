5947529dce2e9c9d8f84f650064edabe553fb6f1
semjan25 <71122843+semjan25@users.noreply.github.com>
2021-02-22 13:47:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import time
 import os
 
 #Read check values
-file1 = open("rules/age.txt", "r")
+file1_leefijt = open("rules/age.txt", "r")
 age_check = int(file1.read())
-file1.close()
+file1leeftijd.close()
 
 file2 = open("rules/height.txt", "r")
 height_check = int(file2.read())
